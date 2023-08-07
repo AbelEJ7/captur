1cf9500f18868f1d7aceb3f8bac1393a3b5ec6c5
abc <abc@example.com>
2023-08-07 15:29:07 -0700

    work page

---
+++ app/work/page.js
@@ -0,0 +1,15 @@
+import Hero from '@/components/Hero'
+import Portfolio from '@/components/Portfolio'
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div>
+       <Hero heading="My Work" message="This is ome of my recent work travling the world" />
+   <Portfolio />
+    </div>
+  
+  )
+}
+
+export default Work
