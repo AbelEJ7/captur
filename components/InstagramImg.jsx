aec487f5b4acb6c7c49da476280d45d8bda5eb81
abc <abc@example.com>
2023-08-07 15:29:29 -0700

    overlay final modification add group class

--- components/InstagramImg.jsx
+++ components/InstagramImg.jsx
@@ -7,8 +7,8 @@ const InstagramImg = ({socialImg}) => {
     <div className='relative'>
         <Image src={socialImg} alt='/' className='w-full h-full'  layout='responsive'/>
         {/* overlay */}
-        <div className='flex justify-center items-center absolute top-0 left-0 right-0 bottom-0 hover:bg-black/70 z-[2]' >
-        <p className='text-gray-300 hidden group-hover:flex'><FaInstagram size={30} /></p>
+        <div className='flex justify-center items-center w-full h-full absolute top-0 left-0 right-0 bottom-0 hover:bg-black/50 z-[2] group' >
+            <p className='text-gray-300  hidden group-hover:block'><FaInstagram size={30} /></p>
          </div>
     </div>
   )
